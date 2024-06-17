255c5a7cba7708af3123cf0c09381a2afc95c67a
RobinM88 <robinmennel88@gmail.com>
2024-06-17 17:29:00 -0500

    add skills controller and repo

---
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/EmployerRepository.java
@@ -0,0 +1,11 @@
+package org.launchcode.techjobs.persistent.models.data;
+
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//2.3.1.a EmployerRepository should extend CrudRepository
+// 2.3.1.b Should be annotated with @Repository
+@Repository
+public interface EmployerRepository extends CrudRepository<Employer, Integer> {
+}
